0f7d1345ad025506d638ba6c61b15a1bd8ecb57a
João Fernando Cosme Bolzani Cheroto <fjoao9744@gmail.com>
2024-07-23 21:23:58 -0300

    Commit 03

--- 10.cpp
+++ 10.cpp
@@ -6,6 +6,4 @@ int main() {
 	cout << teste << endl;
 	cout << teste[0] << endl << teste.at(0);
 
-
-
 }
---
+++ 11.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    string teste = "smogon";
+
+    cout << teste[teste.length() - 1];
+
+
+
+}
