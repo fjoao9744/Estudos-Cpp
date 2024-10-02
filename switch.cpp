0e12ccb1507a6539910cd46c3325aab61a33aec2
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-10-02 11:53:09 -0300

    Create switch.cpp

---
+++ switch.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int dia = 1;
+    
+    switch (dia) {
+        case 1:
+        cout << "domingo";
+        break;
+        
+        case 2:
+        cout << "segunda";
+        break;
+        
+        case 3:
+        cout << "terca";
+        break;
+        
+        case 4:
+        cout << "quarta";
+        break;
+        
+        case 5:
+        cout << "quinta";
+        break;
+        
+        case 6:
+        cout << "sexta";
+        break;
+        
+        case 7:
+        cout << "sabado";
+        break;
+        
+        default:
+        cout << "digite um valor valido";
+        
+    }
+
+}
