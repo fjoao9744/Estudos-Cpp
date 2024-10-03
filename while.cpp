2d7882fb3d4ea82d5ea561ae82feb5052077869a
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-10-03 11:10:25 -0300

    Create while.cpp

---
+++ while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int flag;
+    
+    while (flag <= 10){
+        cout << flag << endl;
+        flag++;
+    }
+    
+    return 0;
+}
