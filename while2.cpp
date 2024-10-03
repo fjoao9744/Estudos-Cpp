4a6d82235341277da9751d13cda21648f1d9d1df
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-10-03 11:11:02 -0300

    Create while2.cpp

---
+++ while2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool flag;
+    
+    while (flag == false){
+        cout << "smogon";
+        flag = true;
+    }
+    
+    return 0;
+}
