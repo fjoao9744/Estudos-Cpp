42ac3d00400dcda4f8d282d82872266f712f4c0b
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-10-07 07:26:28 -0300

    Create matriz2.cpp

---
+++ testes/matriz2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    string matriz[5] = {"smogon", "capa", "x1 freefas", "smogon", "bungas"};
+    
+    for (int i = 0; i < 5; i++) {
+        cout << matriz[i] << endl;
+    }
+}
