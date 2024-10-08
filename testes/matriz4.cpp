899bf3d22a43ea7ae5c4e0121606ddf3e07fa530
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-10-08 13:23:06 -0300

    Create matriz4.cpp

---
+++ testes/matriz4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int matriz[] = {23, 24, 54, 56}; //matriz com numeros indefinidos incialmente
+    
+    for (int i : matriz) {
+        cout << i << endl;
+    }
+}
