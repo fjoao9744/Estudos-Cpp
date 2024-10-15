15a58e9f2557f89b2f0c34047a996fc1ba37278a
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-10-15 15:32:26 -0300

    Create multimatriz.cpp

---
+++ testes/multimatriz.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    int matriz[3][3] = //matrizes multidimencionais = matriz dentro de matriz
+    { // primeira dimenção
+        {1, 2, 3}, //segunda dimenção
+        {4, 5, 6},
+        {7, 8, 9}
+    }; //primeiro numero vai ser quantas sub-matrizes vão ter e o segundo vão ser a quantidade de itens de cada sub-matriz
+    //cada conjunto de colchetes é uma nova dimensão
+    
+    
+}
