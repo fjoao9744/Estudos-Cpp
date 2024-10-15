47e22281721d594e14ea899b2be056d61829ad0e
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-10-15 15:35:16 -0300

    Create multimatriz2.cpp

---
+++ testes/multimatriz2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char matriz[2][2][2] = //uma matriz de 3 dimenções
+    { // dimenção 1
+        { // dimenção 2
+            {'A', 'B'}, // dimenção 3
+            {'C', 'D'}
+        },
+        {
+            {'E', 'F'},
+            {'G', 'H'}
+        }
+    };
+}
+
