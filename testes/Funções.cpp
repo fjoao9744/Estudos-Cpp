0d68be1380421bc04e508c69f1ca9f953e218fb1
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-10-18 21:08:41 -0300

    Create Funções.cpp

---
+++ testes/Funções.cpp
@@ -0,0 +1,19 @@
+include<iostream>
+using namespace std;
+
+int index(int num[], int valor) {
+    int posi = 0;
+    for (int ver : []) {
+    posi++;
+    if (ver == valor) {
+    
+    return posi;
+    }
+    }
+};
+
+int main()
+{
+Index()
+
+}
