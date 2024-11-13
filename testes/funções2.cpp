76125343a2887e25387f3a0de7ee294dc3d9cf8b
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-11-13 07:44:20 -0300

    Create funções2.cpp

---
+++ testes/funções2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int soma(int num1, int num2) // num1 e num2 são os parametros
+{
+    return num1 + num2;
+}
+
+int main()
+{
+    cout << soma(6, 5); // 6 e 5 são os argumentos
+}
