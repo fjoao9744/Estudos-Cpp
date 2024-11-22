d371b07ad002a7f5d7d4bc0ba376b90d687b5cd8
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-11-22 11:25:53 -0300

    Create funções5.cpp

---
+++ testes/funções5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int sum(int n1, int n2) { // a função recebe dois parametros e os soma
+    return n1 + n2;
+}
+
+int main()
+{
+    cout << sum(9, 3);
+}
