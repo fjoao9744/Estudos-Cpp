2e71ca3ae610fa10e0753f5322516fdf4e820b9b
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-11-25 13:45:45 -0300

    Create ponteiros3.cpp

---
+++ testes/ponteiros3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1 = 10;
+    int* ptr = &n1;
+    
+    cout << ptr << endl;
+    cout << n1 << endl;
+    
+    *ptr = 12; // alterando uma variavel pleo ponteiro dela
+    
+    cout << ptr << endl;
+    cout << n1;
+    
+}
